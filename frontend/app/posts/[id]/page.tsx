3a7764f8bfcc8c5bc39b559be99bd79107ac8c77
azubiwa <103619407+azubiwa@users.noreply.github.com>
2025-05-24 14:50:55 +0900

    とりあえず画面

--- frontend/app/posts/[id]/page.tsx
+++ frontend/app/posts/[id]/page.tsx
@@ -1,3 +1,11 @@
+import styles from "../../styles/Post.module.css";
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 type Props = {
   params: {
     id: string;
@@ -8,12 +16,22 @@ export default async function PostPage({ params }: Props) {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`
   );
-  const post = await res.json();
+  const post: Post = await res.json();
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+    <div className={styles.container}>
+      <div className={styles.title}>{post.title}</div>
+      <div className={styles.date}>{post.created_at}</div>
+      <p className={styles.content}>{post.content}</p>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const res = await fetch("https://localhost:3001/api/v1/posts");
+  const posts: Post[] = await res.json();
+
+  return posts.slice(0, 5).map((post) => ({
+    id: post.id.toString(),
+  }));
 }
